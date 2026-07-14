9b990a921316b8c106b3e4f560b5275c0022f26c
neekhseet <iteeshkeen@gmail.ru>
2026-07-14 13:40:21 +0500

    test: zero len if str is null

--- tests/test_proj.cpp
+++ tests/test_proj.cpp
@@ -9,4 +9,10 @@ TEST(ProjTest, CalcStringLength)
 {
     size_t res = string_len(str);
     ASSERT_EQ(11, res);
-}
+};
+
+TEST(ProjTest, NullStringLength)
+{
+    size_t res = string_len(NULL);
+    ASSERT_EQ(0, res);
+};
