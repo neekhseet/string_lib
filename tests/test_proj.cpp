d3e07672922fec146e3a5c0b3fec7601c241b598
neekhseet <iteeshkeen@gmail.ru>
2026-07-14 13:19:13 +0500

    test: calculating length of string

--- tests/test_proj.cpp
+++ tests/test_proj.cpp
@@ -1,4 +1,12 @@
 #include "../includes/string.h"
 #include <gtest/gtest.h>
 
+const char *str = "Abrakadabra";
+
 TEST(ProjTest, Example) { EXPECT_EQ(10, 10); };
+
+TEST(ProjTest, CalcStringLength)
+{
+    size_t res = string_len(&str);
+    ASSERT_EQ(11, res);
+}
