6001b9e2cf1bca96b370e52d419c11961b724ccb
neekhseet <iteeshkeen@gmail.ru>
2026-07-17 16:16:18 +0500

    refactor: add logic to string compare func

--- sources/string.c
+++ sources/string.c
@@ -33,5 +33,26 @@ void string_copy(char **dest, const char *src)
 
 int string_comp(const char *a, const char *b)
 {
-    return -1;
+    if (a == NULL || b == NULL) return 0;
+    if (a == NULL) return -1;
+    if (b == NULL) return 1;
+
+    int i = 0;
+
+    while(a[i] != '\0' && a[i] == b[i] && i < MAX_LEN)
+    {
+        i++;
+    }
+
+    if (i == MAX_LEN) return 0;
+
+    if (a[i] > b[i])
+    {
+        return 1;
+    } else if (a[i] < b[i])
+    {
+        return -1;
+    }
+
+    return 0;
 }
